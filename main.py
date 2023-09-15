074f71205c45578baf69631c298a7eca6e5e29ff
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-09-15 15:41:42 -0400

    Hard code to print all the possible outcomes of rotation along with horizontal and vertical projections

--- main.py
+++ main.py
@@ -7,18 +7,51 @@ A = np.array([
     [13,14,15,16]
     ])
 
-def cwise_rotation(B):
-    transposed_matrix = np.transpose(B)
+def cwise_rotation(P):
+    transposed_matrix = np.transpose(P)
     cwise_rotated_matrix = np.flip(transposed_matrix, axis=1)
     return cwise_rotated_matrix
 
-def projection_horizontal(C):
-    new_matrix = np.flip(C, axis=0)
+def projection_horizontal(P):
+    new_matrix = np.flip(P, axis=0)
     return new_matrix
 
-def projection_vertical(D):
-    new_matrix = np.flip(D, axis=1)
+def projection_vertical(P):
+    new_matrix = np.flip(P, axis=1)
     return new_matrix
 
-print(projection_horizontal(A))
-print(projection_vertical(A))
+M1 = projection_horizontal(A)
+M2 = projection_vertical(A)
+
+B = cwise_rotation(A)
+M3 = projection_horizontal(B)
+M4 = projection_vertical(B) 
+
+C = cwise_rotation(B)
+M5 = projection_horizontal(C)
+M6 = projection_vertical(C)
+
+D = cwise_rotation(C)
+M7 = projection_horizontal(D)
+M8 = projection_vertical(D)
+
+print(M1)
+print("\n")
+print(M2)
+print("\n")
+print(M3)
+print("\n")
+print(M4)
+print("\n")
+print(M5)
+print("\n")
+print(M6)
+print("\n")
+print(M7)
+print("\n")
+print(M8)
+
+
+# print(projection_horizontal(A))
+# print(projection_vertical(A))
+# print()
