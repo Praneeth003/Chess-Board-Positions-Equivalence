c258f45c9701efcbbf8bc6cc670d3bd6e89a5cb3
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-11-05 20:53:20 -0500

    Add code to create files and writing respective positions into it

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from kp import *
 import numpy as np
 
-# posi1 = 'b2K4;2p5;3k4;1Q6;5P2;1B6;16'
-# posi2 = '16;1B6;5P2;1Q6;3k4;2p5;b2K4'
+posi1 = 'b2K4;2p5;3k4;1Q6;5P2;1B6;16'
+posi2 = '16;1B6;5P2;1Q6;3k4;2p5;b2K4'
 
-posi1 = input("Enter the first board position:")
-posi2 = input("Enter the second board position:")
+# posi1 = input("Enter the first board position:")
+# posi2 = input("Enter the second board position:")
 A = np.array(position(posi1))
 X = np.array(position(posi2))
 
@@ -25,6 +25,49 @@ def projection_vertical(P):
     new_matrix = np.flip(P, axis=1)
     return new_matrix
 
+M1 = projection_horizontal(A)
+M2 = projection_vertical(A)
+
+B = cwise_rotation(A)
+M3 = projection_horizontal(B)
+M4 = projection_vertical(B) 
+
+C = cwise_rotation(B)
+M5 = projection_horizontal(C)
+M6 = projection_vertical(C)
+
+D = cwise_rotation(C)
+M7 = projection_horizontal(D)
+M8 = projection_vertical(D)
+
+M = [A, B, C, D, M1, M2, M3, M4]
+
+dictionary = {
+    "A" : A,
+    "B" : B,
+    "C" : C,
+    "D" : D,
+    "M1" : M1,
+    "M2" : M2,
+    "M3" : M3,
+    "M4" : M4
+}
+
+#Function to create a file and write corresponding positions into it.
+def createFile(x,y):
+    # Open a file for writing
+    with open('./data/' + x + '.txt', 'w') as file:
+        for row in y:
+            # Convert row into string
+            rowString = str(row)
+            # Write each row to the file
+            file.write(rowString + '\n')
+
+#Iterating to create all the needed files
+for key, value in dictionary.items():
+    createFile(key, value)
+
+
 # Function to check if there exitsts any pawn in the board 
 def exitsPawn(A):
     for row in A:
@@ -32,47 +75,10 @@ def exitsPawn(A):
             return True
     return False
 
+
 # If pawn exists, only check with the orginal position and its vertical projection position
 # No need to check with other possibilities to preserve the game scenario
-if(exitsPawn(A)):
-    AV = projection_vertical(A)
-    M = [A, AV]
-    isEquivalent = False
-    for i in M:
-        if np.array_equal(X, i):
-            isEquivalent = True
-            break
-    if isEquivalent:
-        print(" There is an equivalence")
-    else:
-        print(" There is no equivalence")
 
-else:
-    M1 = projection_horizontal(A)
-    M2 = projection_vertical(A)
 
-    B = cwise_rotation(A)
-    M3 = projection_horizontal(B)
-    M4 = projection_vertical(B) 
 
-    C = cwise_rotation(B)
-    M5 = projection_horizontal(C)
-    M6 = projection_vertical(C)
 
-    D = cwise_rotation(C)
-    M7 = projection_horizontal(D)
-    M8 = projection_vertical(D)
-
-    M = [A, B, C, D, M1, M2, M3, M4]
-    isEquivalent = False
-
-    for i in M:
-        if np.array_equal(X, i):
-            isEquivalent = True
-            break
-
-    if isEquivalent:
-        print(" There is an equivalence")
-    else:
-        print(" There is no equivalence") 
- 
