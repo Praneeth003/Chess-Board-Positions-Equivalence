3e77342eba2bc304c50c624a0a3e0a874e4e5178
praneeth003 <ch.krishnapraneeth@gmail.com>
2023-12-04 22:23:59 -0500

    choice 1 works

--- main.py
+++ main.py
@@ -1,61 +1,6 @@
 from inputConverter import *
 import numpy as np
 
-print("If you want to check for equivalence between two positions, enter 1")
-print("If you want to check if the position is already in the database, enter 2")
-print("If you want to add a new position to the database, enter 3")
-choice = int(input("Enter your choice: "))
-
-posi1 = input("Enter the first board position:")
-posi2 = input("Enter the second board position:")
-
-A = np.array(position(posi1))
-X = np.array(position(posi2))
-
-# Function to rotate the position of board in clockwise direction
-def cwise_rotation(P):
-    transposed_matrix = np.transpose(P)
-    cwise_rotated_matrix = np.flip(transposed_matrix, axis=1)
-    return cwise_rotated_matrix
-
-# Function to project the board along the horizontal axis
-def projection_horizontal(P):
-    new_matrix = np.flip(P, axis=0)
-    return new_matrix
-
-# Function to project the board along the vertical axis
-def projection_vertical(P):
-    new_matrix = np.flip(P, axis=1)
-    return new_matrix
-
-# All the possible positions assigned to respective variables
-M1 = projection_horizontal(A)
-M2 = projection_vertical(A)
-
-B = cwise_rotation(A)
-M3 = projection_horizontal(B)
-M4 = projection_vertical(B) 
-
-C = cwise_rotation(B)
-M5 = projection_horizontal(C)
-M6 = projection_vertical(C)
-
-D = cwise_rotation(C)
-M7 = projection_horizontal(D)
-M8 = projection_vertical(D)
-
-# Using dictionary to help placing positions in corresponding filenames
-dictionary = {
-    "A" : A,
-    "B" : B,
-    "C" : C,
-    "D" : D,
-    "M1" : M1,
-    "M2" : M2,
-    "M3" : M3,
-    "M4" : M4
-}
-
 # Function to create a file and write corresponding position into it
 def createFile(l, m):
     # Open a file for writing
@@ -65,24 +10,6 @@ def createFile(l, m):
             rowString = str(row)
             # Write each row to the file
             file.write(rowString + '\n')
-
-# iterating to create all the respective files
-for key, value in dictionary.items():
-    createFile(key, value)
-
-
-# Function to check if there exitsts any pawn in the board 
-def exitsPawn(A):
-    for row in A:
-        if 'p' in row or 'P' in row:
-            return True
-    return False
-
-# Converting X into List of Strings format
-Y =[]
-for row in X:
-    Y.append(str(row))
-Y = np.array(Y)
 
 
 # Function to open a file and read position from it
@@ -95,35 +22,107 @@ def readFile(l):
     return np.array(content)
 
 
-# If pawn exists, only check with the orginal position and its vertical projection position
-# No need to check with other possibilities to preserve the game scenario
-if(exitsPawn(A)):
-    M = ["A", "M2"]
-    isEquivalent = False
-    
-    for i in M:
-        if np.array_equal(Y, readFile(i)):
-            isEquivalent = True
-            break
-    
-    if isEquivalent:
-        print("There is an equivalence!")
-    else:
-        print("There is no equivalence!")
 
-#If there is no pawn, then check with all the possibiltie positions 
-else:
-    M = ["A","B","C","D","M1","M2","M3","M4"]
-    isEquivalent = False
+print("If you want to check for equivalence between two positions, enter 1")
+print("If you want to check if the position is already in the database, enter 2")
+print("If you want to add a new position to the database, enter 3")
 
-    for i in M:
-        if np.array_equal(Y,readFile(i)):
-            isEquivalent = True
-            break
-    if isEquivalent:
-        print("Thre is an equivalence!")
+choice = int(input("Enter your choice: "))
+if choice == 1:
+    posi1 = input("Enter the first board position:")
+    posi2 = input("Enter the second board position:")
+    A = np.array(position(posi1))
+    X = np.array(position(posi2))
+    # Function to rotate the position of board in clockwise direction
+    def cwise_rotation(P):
+        transposed_matrix = np.transpose(P)
+        cwise_rotated_matrix = np.flip(transposed_matrix, axis=1)
+        return cwise_rotated_matrix
+
+    # Function to project the board along the horizontal axis
+    def projection_horizontal(P):
+        new_matrix = np.flip(P, axis=0)
+        return new_matrix
+
+    # Function to project the board along the vertical axis
+    def projection_vertical(P):
+        new_matrix = np.flip(P, axis=1)
+        return new_matrix
+
+    # All the possible positions assigned to respective variables
+    M1 = projection_horizontal(A)
+    M2 = projection_vertical(A)
+
+    B = cwise_rotation(A)
+    M3 = projection_horizontal(B)
+    M4 = projection_vertical(B) 
+
+    C = cwise_rotation(B)
+    M5 = projection_horizontal(C)
+    M6 = projection_vertical(C)
+
+    D = cwise_rotation(C)
+    M7 = projection_horizontal(D)
+    M8 = projection_vertical(D)
+
+    # Using dictionary to help placing positions in corresponding filenames
+    dictionary = {
+        "A" : A,
+        "B" : B,
+        "C" : C,
+        "D" : D,
+        "M1" : M1,
+        "M2" : M2,
+        "M3" : M3,
+        "M4" : M4
+    }
+
+    # Iterating to create all the respective files
+    for key, value in dictionary.items():
+        createFile(key, value)
+
+    # Function to check if there exitsts any pawn in the board 
+    def exitsPawn(A):
+        for row in A:
+            if 'p' in row or 'P' in row:
+                return True
+        return False
+
+    # Converting X into List of Strings format
+    Y =[]
+    for row in X:
+        Y.append(str(row))
+    Y = np.array(Y)
+
+    # If pawn exists, only check with the orginal position and its vertical projection position
+    # No need to check with other possibilities to preserve the game scenario
+    if(exitsPawn(A)):
+        M = ["A", "M2"]
+        isEquivalent = False
+        
+        for i in M:
+            if np.array_equal(Y, readFile(i)):
+                isEquivalent = True
+                break
+        
+        if isEquivalent:
+            print("There is an equivalence!")
+        else:
+            print("There is no equivalence!")
+
+    #If there is no pawn, then check with all the possibiltie positions 
     else:
-        print("There is no equivalence!")
+        M = ["A","B","C","D","M1","M2","M3","M4"]
+        isEquivalent = False
+
+        for i in M:
+            if np.array_equal(Y,readFile(i)):
+                isEquivalent = True
+                break
+        if isEquivalent:
+            print("Thre is an equivalence!")
+        else:
+            print("There is no equivalence!")
 
 
 
